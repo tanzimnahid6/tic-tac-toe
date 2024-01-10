766d88cf6b9121da4a8903add8a31026cab29297
jubayer <tanzimnahid6@gmail.com>
2024-01-10 17:54:59 +0600

    board and toggle done

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
 import { useState } from "react";
 
 /* eslint-disable react/prop-types */
-const Square = () => {
-  const [value, setValue] = useState(null);
-  const handleCLick = () => {
-    setValue('X')
-    console.log("clicked");
-  };
+const Square = ({ value, onSquireClick }) => {
+  console.log("sq");
+
   return (
     <button
-      onClick={handleCLick}
+      onClick={onSquireClick}
       className="border-2 border-blue-800  w-12 h-12 leading-9 bg-white text-lg m-1"
     >
       {value}
@@ -18,25 +15,72 @@ const Square = () => {
 };
 
 const Board = () => {
-  console.log('board')
+  const [squires, setSquires] = useState(Array(9).fill(null));
+  const [x, setX] = useState(true);
+  console.log(squires);
+
+  const handleClick = (i) => {
+    if(squires[i]){
+      alert('Already fill up')
+      return 
+    }
+    const newSquires = [...squires];
+    //state change immutably==== = = = ==  == ===============
+    if (x) {
+      newSquires[i] = "X";
+    } else {
+      newSquires[i] = "O";
+    }
+    
+    setSquires(newSquires);
+    setX(!x);
+    
+  };
   return (
     <>
       <div className="flex">
-        <Square></Square>
-        <Square></Square>
-        <Square></Square>
+        <Square
+          value={squires[0]}
+          onSquireClick={() => handleClick(0)}
+        ></Square>
+        <Square
+          value={squires[1]}
+          onSquireClick={() => handleClick(1)}
+        ></Square>
+        <Square
+          value={squires[2]}
+          onSquireClick={() => handleClick(2)}
+        ></Square>
       </div>
 
       <div className="flex">
-        <Square></Square>
-        <Square></Square>
-        <Square></Square>
+        <Square
+          value={squires[3]}
+          onSquireClick={() => handleClick(3)}
+        ></Square>
+        <Square
+          value={squires[4]}
+          onSquireClick={() => handleClick(4)}
+        ></Square>
+        <Square
+          value={squires[5]}
+          onSquireClick={() => handleClick(5)}
+        ></Square>
       </div>
 
       <div className="flex">
-        <Square></Square>
-        <Square></Square>
-        <Square></Square>
+        <Square
+          value={squires[6]}
+          onSquireClick={() => handleClick(6)}
+        ></Square>
+        <Square
+          value={squires[7]}
+          onSquireClick={() => handleClick(7)}
+        ></Square>
+        <Square
+          value={squires[8]}
+          onSquireClick={() => handleClick(8)}
+        ></Square>
       </div>
     </>
   );
